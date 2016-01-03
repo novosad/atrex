100104d31b8fdca58a819e675c1e7dc0c59f7fa1
novosad <nik_neman@mail.ru>
2016-01-03 14:32:14 +0300

    Edit height site

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -17,11 +17,12 @@ $this->params['breadcrumbs'][] = $this->title;
         <img src="/images/phone.png" alt="phone"> +375(29) 987-65-43 <br/>
         <img src="/images/skype.png" alt="skype"> <a href="skype:turkulec?call"> turkulec </a> <br/>
         <img src="/images/icq.png" alt="icq"> 123456789
-        <img src="http://wwp.icq.com/scripts/online.dll?icq=123456789&img=N" border="0" class="status-icq" /> <br/><br/>
+        <img src="http://wwp.icq.com/scripts/online.dll?icq=123456789&img=N" border="0" class="status-icq"/> <br/><br/>
 
         Фактический адрес: <br/>
         231778, гп. Большая Берестовица, ул.Ленина, 1, каб. 19 <br/><br/>
-        <script type="text/javascript" charset="utf-8" src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=tco8SXXAwaIw7b5ReDf9r2b3OVtQM9Oh&width=500&height=400&lang=ru_RU&sourceType=constructor"></script>
+        <script type="text/javascript" charset="utf-8"
+                src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=tco8SXXAwaIw7b5ReDf9r2b3OVtQM9Oh&width=500&height=400&lang=ru_RU&sourceType=constructor"></script>
 
     </p>
 </div>
